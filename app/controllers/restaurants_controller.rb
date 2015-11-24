57b84ad4fdd11f99e811a997d55e793f11972b58
Eleni Skouroupathi <eleni59@hotmail.com>
2015-11-24 11:40:35 +0000

    added update restaurant feature

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,7 +5,7 @@ class RestaurantsController < ApplicationController
 	end
 
 	def new
-
+		@restaurant = Restaurant.new
 	end
 
 	def create
@@ -20,4 +20,15 @@ class RestaurantsController < ApplicationController
 	def show
 		@restaurant = Restaurant.find(params[:id])
 	end
+
+	def edit
+		@restaurant = Restaurant.find(params[:id])
+	end
+
+	def update
+		@restaurant = Restaurant.find(params[:id])
+		@restaurant.update(restaurant_params)
+		redirect_to '/restaurants'
+	end
+
 end
